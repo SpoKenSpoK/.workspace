05edcd6d5f2f5f9300b0cb6f5dff09cf63792f4a
SpoKenSpoK <spoken.spok@gmail.com>
2015-11-17 23:48:41 +0100

    L'Horloge marche enfin !

--- java/tp6/Horloge.java
+++ java/tp6/Horloge.java
@@ -1,8 +1,22 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Horloge {
 	public static void main(String [] arg){
 		FenetreGraphique fenetre;
-		fenetre = new FenetreGraphique("tp6");
-	
+		fenetre = new FenetreGraphique("The Bruh Clock");
+		Horloge(fenetre);
+
+	}
+
+	public static void Horloge(final FenetreGraphique f){
+		Timer timer;
+		TimerTask task = new TimerTask(){ 
+			public void run(){ f.repaint(); }
+		};
+
+		timer = new Timer();
+		timer.scheduleAtFixedRate(task, 0, 1000);
 	}
 	
 
--- java/tp6/ZoneDessin.java
+++ java/tp6/ZoneDessin.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
 
 public class ZoneDessin extends JPanel{
 	private Image img;
@@ -15,7 +17,7 @@ public class ZoneDessin extends JPanel{
 	public void paint(Graphics g){
 		affiche_fond(g);
 		affiche_graduations(g);
-		//affiche_aiguilles(g);
+		affiche_aiguilles(g);
 	}
 	
 	public void affiche_fond(Graphics g){
@@ -27,22 +29,73 @@ public class ZoneDessin extends JPanel{
 	}
 	
 	public void affiche_graduations(Graphics g){
-		int xOrigin = getSize().width/2;
-		int yOrigin = getSize().height/2;
-		
-		System.out.println(xOrigin);
-		System.out.println(yOrigin);
+		int xOrigin = getWidth()/2;
+		int yOrigin = getHeight()/2;
 		
 		int angle = 360/12;
-		int radius = 120;
+		int radius = 125;
 		
 		for(int i=1; i<=12 ;++i){
 			g.setColor(Color.black);
 			g.drawString(Integer.toString(i), xOrigin+(int)(Math.cos(Math.toRadians((i-3)*(-angle)))*radius), yOrigin-(int)(Math.sin(Math.toRadians((i-3)*(-angle)))*radius));
+			/*g.setColor(Color.green);
+			drawCenteredString(Integer.toString(i), xOrigin+(int)(Math.cos(Math.toRadians((i-3)*(-angle)))*radius), yOrigin-(int)(Math.sin(Math.toRadians((i-3)*(-angle)))*radius), g);
+			*/
+			g.drawLine( xOrigin+(int)(0.75*(Math.cos(Math.toRadians((i-3)*(-angle)))*radius)), 
+						yOrigin-(int)(0.75*(Math.sin(Math.toRadians((i-3)*(-angle)))*radius)),
+						xOrigin+(int)(0.90*(Math.cos(Math.toRadians((i-3)*(-angle)))*radius)),
+						yOrigin-(int)(0.90*(Math.sin(Math.toRadians((i-3)*(-angle)))*radius)) );
+		
 		}
 		
 		// http://www.java2s.com/Tutorial/Java/0261__2D-Graphics/Centertext.htm
 		
 	}
+
+	public void affiche_aiguilles(Graphics g){
+
+		Calendar date = new GregorianCalendar();
+		int hours = date.get(Calendar.HOUR_OF_DAY);
+		int minutes = date.get(Calendar.MINUTE);
+		int secondes = date.get(Calendar.SECOND);
+
+
+		Graphics2D gg = (Graphics2D) g;
+
+		//Hours
+		BasicStroke line = new BasicStroke(10.0f);
+		gg.setStroke(line);
+		gg.setColor(Color.red);
+		gg.drawLine(getWidth()/2, 
+					getHeight()/2,  
+					getWidth()/2+(int)(0.75*(Math.cos(Math.toRadians((hours-3)*(-30)))*125)), 
+					getHeight()/2-(int)(0.75*(Math.sin(Math.toRadians((hours-3)*(-30)))*125)) );
+
+		//Minutes
+		line = new BasicStroke(5.0f);
+		gg.setStroke(line);
+		gg.setColor(Color.blue);
+		gg.drawLine(getWidth()/2, 
+					getHeight()/2,  
+					getWidth()/2+(int)(0.75*(Math.cos(Math.toRadians((minutes-15)*(-360/60)))*125)), 
+					getHeight()/2-(int)(0.75*(Math.sin(Math.toRadians((minutes-15)*(-360/60)))*125)) );
+
+		//Secondes
+		line = new BasicStroke(1.0f);
+		gg.setStroke(line);
+		gg.setColor(Color.black);
+		gg.drawLine(getWidth()/2, 
+					getHeight()/2,  
+					getWidth()/2+(int)(0.75*(Math.cos(Math.toRadians((secondes-15)*(-360/60)))*125)), 
+					getHeight()/2-(int)(0.75*(Math.sin(Math.toRadians((secondes-15)*(-360/60)))*125)) );
+
+	}
+	/*
+	public void drawCenteredString(String s, int w, int h, Graphics g) {
+    	FontMetrics fm = g.getFontMetrics();
+    	int x = (w - fm.stringWidth(s));
+    	//int y = (fm.getAscent() + (h - (fm.getAscent() + fm.getDescent())));
+    	g.drawString(s, x, h);
+  	}  */
 
 }
